b15edb4375bd5c9336da31498a9305694b829157
Amal S <AmalSajiv>
2022-08-27 19:55:51 +0530

    bestseller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Best-Seller';
+
+  constructor(){}
+
+  ngOnInit() {
+    
+  }
+
+  url:string = "../assets/img4.jpg";
+  changeImage(event:any){
+    this.url = event.target.src
+  }
+
 }
